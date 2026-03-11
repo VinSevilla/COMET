13fe4c9ad9fe0c0b2eca87bc7b15f2711e5383e9
VinSevilla <vls.sevilla@gmail.com>
2026-03-11 16:05:40 -0700

    feat: route and show onboarding

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { Redirect } from "expo-router";
+import { useState } from "react";
+
+export default function Index() {
+  const [isLoggedIn] = useState(false); // toggle to true to test tab navigation
+
+  return <Redirect href={isLoggedIn ? "/(tabs)" : "/(auth)/onboarding"} />;
+}

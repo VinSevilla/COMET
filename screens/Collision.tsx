65a16c3fdd0c2680176044966c0c1d5254026ab5
VinSevilla <vls.sevilla@gmail.com>
2026-03-11 15:22:19 -0700

    feat: add skeleton screens

---
+++ screens/Collision.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Collision() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 18 }}>Collision screen</Text>
+    </View>
+  );
+}
---
+++ screens/SignUp.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function SignUp() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 18 }}>Sign Up screen</Text>
+    </View>
+  );
+}
